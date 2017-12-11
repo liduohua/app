13e223e9c178bf58a44632fae144f1030f8e14cf
ldh <1114198200@qq.com>
2017-12-11 19:53:40 +0800

    modify App.vue file

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,20 @@
 <script>
 	import Index from './views/Index.vue';
 	export default {
-		components :{rrr
+		methods : {
+			resizeFont(){
+				var htmlEl = document.documentElement;
+				let deviceWidth = htmlEl.getBoundingClientRect().width;
+        		if(deviceWidth > 750) deviceWidth = 750;
+        		htmlEl.style.fontSize = deviceWidth / 7.5 + 'px';
+			},
+		},
+		mounted(){ 
+			//alert(this.resizeFont);
+			this.resizeFont();
+		},
+		components : {
 			Index
-		}
+		},
 	}
 </script>
